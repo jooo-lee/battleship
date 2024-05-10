41c52c67d853394386e0ca1df7c6fe8c41c21818
Joseph Lee <josephlee3004@gmail.com>
2024-05-10 16:07:21 -0400

    Extract out capping number of hits test

--- test/ship.test.js
+++ test/ship.test.js
@@ -18,12 +18,18 @@ describe('ship class', () => {
 
         ship.hit();
         expect(ship).toHaveProperty('hits', 4);
-
-        ship.hit();
-        expect(ship).toHaveProperty('hits', 4);
     });
 
-    test('is sunk if hits greater than length', () => {
+    test('caps number of hits at ship length', () => {
+        const ship = new Ship(3);
+        ship.hit();
+        ship.hit();
+        ship.hit();
+        ship.hit();
+        expect(ship).toHaveProperty('hits', 3);
+    });
+
+    test('is sunk if number of hits equals ship length', () => {
         const ship = new Ship(2);
         expect(ship.isSunk()).toBe(false);
         ship.hit();
