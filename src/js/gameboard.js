d711897411b988f801fff065b60dbc31caff42a0
Joseph Lee <josephlee3004@gmail.com>
2024-05-12 17:53:19 -0400

    Refactor placeShip method

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -2,6 +2,7 @@ import Ship from './ship';
 
 class Gameboard {
     constructor() {
+        // 10x10 grid
         this.board = [...Array(10)].map(() => Array(10).fill(null));
     }
 
@@ -9,87 +10,74 @@ class Gameboard {
         return this.board;
     }
 
-    placeShip(start, end) {
-        const [row1, col1] = start;
-        const [row2, col2] = end;
-
+    // Check if coordinate is within bounds of our board
+    #checkOnBoard(coordinate) {
+        const [row, col] = coordinate;
         if (
-            row1 < 0 ||
-            row1 > 9 ||
-            col1 < 0 ||
-            col1 > 9 ||
-            row2 < 0 ||
-            row2 > 9 ||
-            col2 < 0 ||
-            col2 > 9
+            row < 0 ||
+            row > this.board.length - 1 ||
+            col < 0 ||
+            col > this.board.length - 1
         ) {
-            throw new Error('Ship coordinates out of bounds!');
+            throw new Error('Coordinate out of bounds!');
         }
+    }
 
-        if (row1 === row2) {
-            const shipLength = Math.abs(col2 - col1);
-            const ship = new Ship(shipLength);
-            if (col1 < col2) {
-                // Places ship horizontally left to right
-                for (let j = col1; j <= col2; j++) {
-                    // Check that coordinates are unoccupied
-                    if (this.board[row1][j]) {
-                        for (j = j - 1; j >= col1; j--) {
-                            // Undo new ship placement
-                            this.board[row1][j] = null;
-                        }
-                        throw new Error('Cannot stack ships!');
-                    }
-
-                    this.board[row1][j] = ship;
+    // Helper method for placeShip
+    #placeShipHorizontally(startCoordinate, endCoordinate) {
+        let [row, col1] = startCoordinate;
+        let col2 = endCoordinate[1];
+        /**
+         * Ensure col1 <= col2 since we are incrementing from col1 to col2 in
+         * the for loop below to place the ship.
+         */
+        if (col1 > col2) {
+            [col1, col2] = [col2, col1];
+        }
+        const ship = new Ship(col2 - col1);
+        for (let j = col1; j <= col2; j++) {
+            // Undo new ship placement if we reach occupied coordinates
+            if (this.board[row][j]) {
+                for (j = j - 1; j >= col1; j--) {
+                    this.board[row][j] = null;
                 }
-            } else {
-                // Places ship horizontally right to left
-                for (let j = col2; j <= col1; j++) {
-                    // Check that coordinates are unoccupied
-                    if (this.board[row1][j]) {
-                        for (j = j - 1; j >= col2; j--) {
-                            // Undo new ship placement
-                            this.board[row1][j] = null;
-                        }
-                        throw new Error('Cannot stack ships!');
-                    }
-
-                    this.board[row1][j] = ship;
-                }
+                throw new Error('Cannot stack ships!');
             }
-        } else if (col1 === col2) {
-            const shipLength = Math.abs(row2 - row1);
-            const ship = new Ship(shipLength);
-            if (row1 < row2) {
-                const ship = new Ship(shipLength);
-                for (let i = row1; i <= row2; i++) {
-                    // Check that coordinates are unoccupied
-                    if (this.board[i][col1]) {
-                        for (i = i - 1; i >= row1; i--) {
-                            // Undo new ship placement
-                            this.board[i][col1] = null;
-                        }
-                        throw new Error('Cannot stack ships!');
-                    }
+            this.board[row][j] = ship;
+        }
+    }
 
-                    this.board[i][col1] = ship;
+    // Helper method for placeShip
+    #placeShipVertically(startCoordinate, endCoordinate) {
+        let [row1, col] = startCoordinate;
+        let row2 = endCoordinate[0];
+        /**
+         * Ensure row1 <= row2 since we are incrementing from row1 to row2 in
+         * the for loop below to place the ship.
+         */
+        if (row1 > row2) {
+            [row1, row2] = [row2, row1];
+        }
+        const ship = new Ship(row2 - row1);
+        for (let i = row1; i <= row2; i++) {
+            // Undo new ship placement if we reach occupied coordinates
+            if (this.board[i][col]) {
+                for (i = i - 1; i >= row1; i--) {
+                    this.board[i][col] = null;
                 }
-            } else {
-                // Places ship vertically bottom to top
-                for (let i = row2; i <= row1; i++) {
-                    // Check that coordinates are unoccupied
-                    if (this.board[i][col1]) {
-                        for (i = i - 1; i >= row2; i--) {
-                            // Undo new ship placement
-                            this.board[i][col1] = null;
-                        }
-                        throw new Error('Cannot stack ships!');
-                    }
-
-                    this.board[i][col1] = ship;
-                }
+                throw new Error('Cannot stack ships!');
             }
+            this.board[i][col] = ship;
+        }
+    }
+
+    placeShip(startCoordinate, endCoordinate) {
+        this.#checkOnBoard(startCoordinate);
+        this.#checkOnBoard(endCoordinate);
+        if (startCoordinate[0] === endCoordinate[0]) {
+            this.#placeShipHorizontally(startCoordinate, endCoordinate);
+        } else if (startCoordinate[1] === endCoordinate[1]) {
+            this.#placeShipVertically(startCoordinate, endCoordinate);
         } else {
             throw new Error('Ships must be placed horizontally or vertically!');
         }
