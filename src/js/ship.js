944bb201271e711673c42a5bb66f71ca47c5e7e2
Joseph Lee <josephlee3004@gmail.com>
2024-05-14 18:24:58 -0400

    Use getters to access ship properties

--- src/js/ship.js
+++ src/js/ship.js
@@ -1,20 +1,31 @@
 class Ship {
+    #length;
+    #hits;
+
     constructor(length) {
         if (length < 2 || length > 5) {
             throw new Error('Ship length must be between 2 and 5!');
         }
-        this.length = length;
-        this.hits = 0;
+        this.#length = length;
+        this.#hits = 0;
+    }
+
+    get length() {
+        return this.#length;
+    }
+
+    get hits() {
+        return this.#hits;
     }
 
     // Increase number of 'hits' on ship, cap at length
     hit() {
-        if (this.hits < this.length) this.hits++;
+        if (this.#hits < this.#length) this.#hits++;
     }
 
     // Calculate whether or not ship is sunk based on its length and hits received
     isSunk() {
-        return this.hits >= this.length;
+        return this.#hits >= this.#length;
     }
 }
 
