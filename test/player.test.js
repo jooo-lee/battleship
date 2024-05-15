7a84e9962478f181c8cd3beaef4dfdfd1d7083b1
Joseph Lee <josephlee3004@gmail.com>
2024-05-15 14:12:36 -0400

    Have player objects contain their own gameboards

--- test/player.test.js
+++ test/player.test.js
@@ -1,5 +1,6 @@
 import { expect, test } from '@jest/globals';
 import Player from '../src/js/player';
+import Gameboard from '../src/js/gameboard';
 
 test('is either of type human or computer', () => {
     expect(() => {
@@ -14,4 +15,14 @@ test('is either of type human or computer', () => {
     expect(() => {
         new Player('barbell');
     }).toThrow();
+});
+
+test('contains its own gameboard', () => {
+    const human = new Player('human');
+    expect(human.gameboard).toBeInstanceOf(Gameboard);
+
+    const computer = new Player('computer');
+    expect(computer.gameboard).toBeInstanceOf(Gameboard);
+
+    expect(computer.gameboard).not.toBe(human.gameboard);
 });
