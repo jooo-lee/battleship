8fc5dbb4c004f3a2e52afa1764b7dcc665078bd3
Joseph Lee <josephlee3004@gmail.com>
2024-05-17 14:14:49 -0400

    Implement turn-based game logic with DOM

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,15 +1,31 @@
-const renderBoard = (container, board, shipsVisible) => {
+const renderBoard = (container, board, isEnemy, sqClickCb = null) => {
+    if (isEnemy && !sqClickCb) {
+        throw new Error('Callback not provided for enemy board!');
+    }
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[0].length; j++) {
             const square = document.createElement('div');
             square.classList.add('square');
-            if (shipsVisible && board[i][j]) {
+            square.dataset.row = i;
+            square.dataset.column = j;
+            if (!isEnemy && board[i][j]) {
                 // Make ship visible in DOM
                 square.classList.add('ship');
+            }
+            if (isEnemy) {
+                square.addEventListener('click', sqClickCb);
             }
             container.appendChild(square);
         }
     }
 };
 
-export { renderBoard };
+const markMiss = (square) => {
+    square.classList.add('miss');
+};
+
+const markHit = (square) => {
+    square.classList.add('hit');
+};
+
+export { renderBoard, markMiss, markHit };
--- src/js/runGame.js
+++ src/js/runGame.js
@@ -1,31 +1,85 @@
-import { renderBoard } from './dom';
+import { renderBoard, markMiss, markHit } from './dom';
 import Player from './player';
 
+const playerOne = new Player('human');
+const playerOneGameboard = playerOne.gameboard;
+
+const playerTwo = new Player('computer');
+const playerTwoGameboard = playerTwo.gameboard;
+
+// TODO
+const checkGameOver = () => {
+    if (playerOneGameboard.allShipsSunk) {
+        console.log('p2 wins!');
+        // Stop game from continuing
+    } else if (playerTwoGameboard.allShipsSunk) {
+        console.log('p1 wins!');
+        // Stop game from continuing
+    }
+};
+
+const takeTurn = (e) => {
+    // Player one's turn
+    const row = e.target.dataset.row;
+    const col = e.target.dataset.column;
+    if (playerTwoGameboard.receiveAttack([row, col])) {
+        if (playerTwoGameboard.board[row][col]) {
+            // Ship present at [row, col]
+            markHit(e.target);
+            checkGameOver();
+        } else {
+            markMiss(e.target);
+        }
+    } else {
+        return;
+    }
+
+    // Player two's turn
+    setTimeout(() => {
+        let randomRow = Math.floor(Math.random() * 10);
+        let randomCol = Math.floor(Math.random() * 10);
+
+        while (!playerOneGameboard.receiveAttack([randomRow, randomCol])) {
+            randomRow = Math.floor(Math.random() * 10);
+            randomCol = Math.floor(Math.random() * 10);
+        }
+
+        const attackedSquare = document.querySelector(
+            `#player-one-board > .square:nth-child(${randomRow * 10 + randomCol + 1})`
+        );
+
+        if (playerOneGameboard.board[randomRow][randomCol]) {
+            // Ship present at [randomRow, randomCol]
+            markHit(attackedSquare);
+            checkGameOver();
+        } else {
+            markMiss(attackedSquare);
+        }
+    }, 1000);
+};
+
 const runGame = () => {
-    const playerOne = new Player('human');
-    const playerOneBoard = playerOne.gameboard;
-    playerOneBoard.placeShip([0, 0], [0, 4]);
-    playerOneBoard.placeShip([1, 5], [2, 5]);
-    playerOneBoard.placeShip([4, 7], [7, 7]);
-    playerOneBoard.placeShip([8, 1], [8, 3]);
-    playerOneBoard.placeShip([9, 5], [9, 7]);
+    playerOneGameboard.placeShip([0, 0], [0, 4]);
+    playerOneGameboard.placeShip([1, 5], [2, 5]);
+    playerOneGameboard.placeShip([4, 7], [7, 7]);
+    playerOneGameboard.placeShip([8, 1], [8, 3]);
+    playerOneGameboard.placeShip([9, 5], [9, 7]);
     renderBoard(
         document.querySelector('#player-one-board'),
-        playerOneBoard.board,
-        true
+        playerOneGameboard.board,
+        false
     );
 
-    const playerTwo = new Player('computer');
-    const playerTwoBoard = playerTwo.gameboard;
-    playerTwoBoard.placeShip([4, 1], [4, 5]);
-    playerTwoBoard.placeShip([1, 8], [1, 9]);
-    playerTwoBoard.placeShip([4, 9], [7, 9]);
-    playerTwoBoard.placeShip([6, 4], [8, 4]);
-    playerTwoBoard.placeShip([6, 6], [8, 6]);
+    playerTwoGameboard.placeShip([4, 1], [4, 5]);
+    playerTwoGameboard.placeShip([1, 8], [1, 9]);
+    playerTwoGameboard.placeShip([4, 9], [7, 9]);
+    playerTwoGameboard.placeShip([6, 4], [8, 4]);
+    playerTwoGameboard.placeShip([6, 6], [8, 6]);
     renderBoard(
         document.querySelector('#player-two-board'),
-        playerTwoBoard.board,
-        true
+        playerTwoGameboard.board,
+        true,
+        takeTurn
     );
 };
 
